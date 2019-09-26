761922aa7e2d509fb607b781650ab21abc45241a
lucianoravainera <lucho.barracas@hotmail.com>
2019-09-26 18:56:43 -0300

    tp2 compila ok

---
+++ TP-02/Entidades/Leche.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades_2018
+{
+    public class Leche : Producto
+    {
+        public enum ETipo { Entera, Descremada }
+        ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+            tipo = ETipo.Entera;
+        }
+        /// <summary>
+        /// Constructor que toma 4 parametros
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="codigoDeBarras"></param>
+        /// <param name="colorPrimarioEmpaque"></param>
+        /// <param name="tipo"></param>
+        public Leche(EMarca marca, string codigoDeBarras, ConsoleColor colorPrimarioEmpaque, ETipo tipo) : this(marca, codigoDeBarras, colorPrimarioEmpaque)
+        {
+            this.tipo = tipo;
+        }
+
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
